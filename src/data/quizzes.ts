9e4368943144b7d1ca6958be25090e33bde3c1fb
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-04-01 12:40:56 +0300

    add quiz 9 and format all files.

--- src/data/quizzes.ts
+++ src/data/quizzes.ts
@@ -6,55 +6,61 @@ import lec7 from './lec7.json';
 import lec8 from './lec8.json';
 import lec9 from './lec9.json';
 import lec10 from './lec10.json';
-
+import lec11 from './lec11.json';
 
 export const quizzes = [
   {
-    id: "quiz-1",
-    title: "Κουίζ 1",
-    description: "",
-    questions: lec1
+    id: 'quiz-1',
+    title: 'Κουίζ 1',
+    description: '',
+    questions: lec1,
   },
   {
-    id: "quiz-2",
-    title: "Κουίζ 2",
-    description: "",
-    questions: lec4
+    id: 'quiz-2',
+    title: 'Κουίζ 2',
+    description: '',
+    questions: lec4,
   },
   {
-    id: "quiz-3",
-    title: "Κουίζ 3",
-    description: "",
-    questions: lec5
+    id: 'quiz-3',
+    title: 'Κουίζ 3',
+    description: '',
+    questions: lec5,
   },
   {
-    id: "quiz-4",
-    title: "Κουίζ 4",
-    description: "",
-    questions: lec6
+    id: 'quiz-4',
+    title: 'Κουίζ 4',
+    description: '',
+    questions: lec6,
   },
   {
-    id: "quiz-5",
-    title: "Κουίζ 5",
-    description: "",
-    questions: lec7
+    id: 'quiz-5',
+    title: 'Κουίζ 5',
+    description: '',
+    questions: lec7,
   },
   {
-    id: "quiz-6",
-    title: "Κουίζ 6",
-    description: "",
-    questions: lec8
+    id: 'quiz-6',
+    title: 'Κουίζ 6',
+    description: '',
+    questions: lec8,
   },
   {
-    id: "quiz-7",
-    title: "Κουίζ 7",
-    description: "",
-    questions: lec9
+    id: 'quiz-7',
+    title: 'Κουίζ 7',
+    description: '',
+    questions: lec9,
   },
   {
-    id: "quiz-8",
-    title: "Κουίζ 8",
-    description: "",
-    questions: lec10
+    id: 'quiz-8',
+    title: 'Κουίζ 8',
+    description: '',
+    questions: lec10,
+  },
+  {
+    id: 'quiz-9',
+    title: 'Κουίζ 9',
+    description: '',
+    questions: lec11,
   }
 ];
